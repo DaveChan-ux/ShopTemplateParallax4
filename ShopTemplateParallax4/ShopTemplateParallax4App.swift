ce77d76ab6460d02a67e2e1514355e018bc28afa
DaveChan-ux <david.chan@rewardstyle.com>
2025-01-14 15:31:33 -0600

    Initial Commit

---
+++ ShopTemplateParallax4/ShopTemplateParallax4App.swift
@@ -0,0 +1,17 @@
+//
+//  ShopTemplateParallax4App.swift
+//  ShopTemplateParallax4
+//
+//  Created by Dave.Chan on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopTemplateParallax4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
